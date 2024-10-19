59c3bb11e88031f89a02a9dd6d6ddc95e36506ef
archery <omrigamer100@gmail.com>
2024-10-19 17:04:40 +0300

    test

--- nodes.sql
+++
@@ -1,8 +0,0 @@
-create table nodes (
-	node_id SERIAL PRIMARY KEY,
-	parent INTEGER REFERENCES nodes(node_id),
-	title VARCHAR(50),
-	description VARCHAR(50),
-	status VARCHAR(50) DEFAULT 'expired',
-	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);

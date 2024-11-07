b4aa6d1dc26ff0be042c812f984d16f5c87ac93c
archery <omrigamer100@gmail.com>
2024-11-07 21:45:06 +0200

    added pings

--- tests/runscript.py
+++ tests/runscript.py
@@ -5,7 +5,8 @@ from threading import Thread
 scripts = [
     "script_one.py",
     "script_two.py",
-    "script_three.py"
+    "script_three.py",
+    "script_four.py",
 ]
 
 def run_script(script_name):

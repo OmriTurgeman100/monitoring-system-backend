3542f65e58912757e82eca8d3a8ee86ab688f332
archery <omrigamer100@gmail.com>
2024-11-13 23:50:36 +0200

    test

--- tests/script_two.py
+++ tests/script_two.py
@@ -4,13 +4,22 @@ import logging
 import json
 from datetime import datetime
 from multiprocessing import Process
+import os
 
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="a")
 
-def black_box_script(report_id, title, description, value):
+def ping(report_id,title, ip, description):
+    output = os.system(f"ping {ip}")
+
+    if output == 0:
+        value = 100
+    else:
+        value = 50
+
+    send_to_api(report_id, title, description, value)
+
+def send_to_api(report_id, title, description, value):
     try:
-        response = requests.get("https://www.udemy.com/")
-        status = response.status_code
 
         api = "http://localhost/api/v1/post/reports"
 
@@ -22,18 +31,19 @@ def black_box_script(report_id, title, description, value):
             "report_id": report_id,
             "title": title,
             "description": description,
-            "value": 404
+            "value": value
         }
 
         response = requests.post(api, data=json.dumps(body), headers=headers)
         print(response.status_code)
+
         current_time = datetime.now()
-        logging.info(f"sent report, report id: {report_id}, title: {title}, description: {description}, value: {value}, time: {current_time}")
+        logging.info(f"sent report, report id: {report_id}, title: {title}, description: {description}, value: {value}, time: {current_time}, status_code: {response.status_code}")
 
     except Exception as e:
+        current_time = datetime.now()
+        logging.error(f("error is {e}, time: {current_time}"))
         print(e)
-
-
 
 if __name__ == "__main__":
     with open("data.json") as pings: # * open the json file once.
@@ -44,8 +54,12 @@ if __name__ == "__main__":
             for item in pings_data:
                 title = item["title"]
                 ip = item["ip"]
+                description = "pinging servers"
+                report_id = f"{title} {ip}"
+                ping(report_id, title, description, ip)
 
-                print(title, ip)
+
+              
 
        
 

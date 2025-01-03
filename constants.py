b331fd6ac29d8558f80de52725aebb932378455d
omri <omrigamer100@gmail.com>
2025-01-03 14:47:25 +0200

    changed the docker file

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
-DB_HOST='host.docker.internal' # ! will use docker internal network to connect to host, almost same as localhost. 
+# DB_HOST='host.docker.internal' # ! will use docker internal network to connect to host, almost same as localhost. 
+DB_HOST='localhost' # ! will use docker internal network to connect to host, almost same as localhost. 
 DB_NAME='postgres' 
 DB_USER='postgres'
 DB_PASS='postgres'
